c38288e36a55ecc51e48d16bedbc2ce2a84a9b61
Marvin Lim <marvzknows03@gmail.com>
2024-12-26 22:28:28 +0800

    refactor and optimize component

---
+++ src/components/Admin/TableRow.tsx
@@ -0,0 +1,71 @@
+import { MdEdit } from "react-icons/md";
+import { MovieRatingsType, MoviesType } from "../../types/movies";
+import { AiFillDelete } from "react-icons/ai";
+
+const RatingText = ({ rating }: { rating: MovieRatingsType }) => {
+  const color = {
+    G: "text-green-500",
+    PG: "text-blue-500",
+    SPG: "text-danger",
+  };
+  return (
+    <div className={`flex items-center font-bold ${color[rating]}`}>
+      {rating}
+    </div>
+  );
+};
+
+type TableRowProps = MoviesType & {
+  HandleEdit: (data: MoviesType) => void;
+  HandleDelete: (id: string) => void;
+  isDeleting: boolean;
+};
+
+const TableRow = (props: TableRowProps) => {
+  return (
+    <tr
+      key={props.id}
+      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+    >
+      <th
+        scope="row"
+        className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        <img
+          className="w-14 object-contain"
+          src={props.image}
+          alt={props.movie_name}
+        />
+        <div className="ps-3">
+          <div className="text-base font-semibold">{props.movie_name}</div>
+          <div className="font-normal text-gray-500">
+            Duration: <small className="font-bold">{props.duration}mins</small>
+          </div>
+        </div>
+      </th>
+      <td className="px-6 py-4">{props.genre}</td>
+      <td className="px-6 py-4">
+        <RatingText rating={props.mtrcb_rating} />
+      </td>
+      <td className="px-6 py-4">
+        <button
+          onClick={() => props.HandleEdit(props)}
+          disabled={props.isDeleting}
+          className="px-2 py-1 bg-blue-700 text-white rounded hover:bg-blue-800 mr-1 font-bold disabled:bg-blue-300"
+        >
+          <MdEdit size={20} />
+        </button>
+        <button
+          disabled={props.isDeleting}
+          onClick={() => props.HandleDelete(props.id)}
+          className="px-2 py-1 bg-red-700 text-white rounded hover:bg-red-800 mr-1 font-bold disabled:bg-red-300"
+        >
+          <AiFillDelete size={20} />
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+// export default memo(TableRow);
+export default TableRow;
